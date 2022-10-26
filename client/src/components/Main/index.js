aa112db2e28012c8e3e4caa3e002fb0366e0bd61
rockwoodc <rockwoodce@gmail.com>
2022-10-26 12:18:13 -0400

    fix file name

---
+++ client/src/components/Main/index.js
@@ -0,0 +1,23 @@
+// import Search from "../../pages/Search"
+// import Contact from "../../pages/Contact"
+// import Homepage from "../../pages/Homepage"
+// import Shop from "../../pages/Shop"
+
+// export default function Main({page}){
+//     function renderPage(){
+//         if(page === 'homepage'){
+//             return(<Homepage/>)
+//         }else if(page ==='contact'){
+//             return (<Contact/>)
+//         }else if(page ==='serch'){
+//             return (<Search/>)
+//         }else(page ==='shop'){
+//             return (<Shop/>)
+//         }
+//     }
+//     return(
+//         <>
+//         {renderPage()}
+//         </>
+//     )
+// }
