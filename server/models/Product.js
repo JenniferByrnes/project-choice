c9f4dae3a265750d42aad681baa0efa13c62ef7e
Jennifer Byrnes <jennifer.byrnes@outlook.com>
2022-10-26 16:24:32 -0400

    adding in models and schemas

--- server/models/Product.js
+++ server/models/Product.js
@@ -1,18 +1,40 @@
-const { Schema } = require("mongoose");
+const mongoose = require('mongoose');
+const { Schema } = mongoose;
 
 //create the Products model using the
 const productSchema = new Schema({
-  productCategory: {
+  name: {
     type: String,
+    required: true,
+    trim: true
   },
-  productPrice: {
-    type: Integral,
+  description: {
+    type: String
+  },
+  image: {
+    type: String
+  },
+  category: {
+    type: Schema.Types.ObjectId,
+    ref: 'Category',
+    required: true
+  },
+  price: {
+    type: Number,
+    required: true,
+    min: 0.99
   },
   size: {
     type: String,
   },
   colors: [],
+  quantity: {
+    type: Number,
+    min: 0,
+    default: 0
+  },
 });
 
 // export the Products models
-module.exports = productSchema;
+const Product = mongoose.model('Product', productSchema);
+module.exports = Product;
--- server/models/User.js
+++ server/models/User.js
@@ -1,8 +1,10 @@
-const { Schema, model } = require("mongoose");
+const mongoose = require('mongoose');
+const { Schema } = mongoose;
 const bcrypt = require("bcrypt");
 
 // import schema from Product.js
-const productSchema = require("./Product");
+//const productSchema = require("./Product");
+const Order = require('./Order');
 
 const userSchema = new Schema(
   {
@@ -20,9 +22,11 @@ const userSchema = new Schema(
     password: {
       type: String,
       required: true,
+      minlength: 5
     },
     // set savedProducts to be an array of data that adheres to the productSchema
-    savedProducts: [productSchema],
+    //savedProducts: [productSchema],
+    orders: [Order.schema],
   },
   // set this to use virtual below
   {
@@ -47,11 +51,5 @@ userSchema.methods.isCorrectPassword = async function (password) {
   return bcrypt.compare(password, this.password);
 };
 
-// when we query a user, we'll also get another field called `bookCount` with the number of saved books we have
-userSchema.virtual("bookCount").get(function () {
-  return this.savedBooks.length;
-});
-
-const User = model("User", userSchema);
-
+const User = mongoose.model("User", userSchema);
 module.exports = User;
