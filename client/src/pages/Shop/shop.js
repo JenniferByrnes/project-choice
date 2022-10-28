2f2e33a1e62575cfa6b6bc417cf59f146e510014
Jennifer Byrnes <jennifer.byrnes@outlook.com>
2022-10-28 11:57:36 -0400

    adding shop front end

--- client/src/pages/Shop/shop.js
+++ client/src/pages/Shop/shop.js
@@ -1,10 +1,14 @@
+import React from "react";
+import ProductList from "../../components/ProductList";
+import CategoryMenu from "../../components/CategoryMenu";
+import Cart from '../../components/Cart';
+
 export default function Shop() {
   return (
-    <div>
-      <h1>this is the shop page</h1>
-      <a href="/">
-        <button type="button">take me home</button>
-      </a>
+    <div className="container">
+      <CategoryMenu />
+      <ProductList />
+      <Cart />
     </div>
   );
 }
