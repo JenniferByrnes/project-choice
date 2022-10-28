e21371c3627f25256958c66dc1363b138c9eac86
Jennifer Byrnes <jennifer.byrnes@outlook.com>
2022-10-28 12:34:15 -0400

    adding shop front end

--- client/src/pages/Shop/shop.js
+++ client/src/pages/Shop/shop.js
@@ -1,4 +1,5 @@
 import React from "react";
+import mainLogo from "../../assets/mainLogo/main-logo.png";
 import ProductList from "../../components/ProductList";
 import CategoryMenu from "../../components/CategoryMenu";
 import Cart from '../../components/Cart';
@@ -6,6 +7,14 @@ import Cart from '../../components/Cart';
 export default function Shop() {
   return (
     <div className="container">
+      <div>
+        <img
+          src={mainLogo}
+          className="my-2 pic float-left"
+          style={{ width: "50%" }}
+          alt="cover"
+        />
+      </div>
       <CategoryMenu />
       <ProductList />
       <Cart />
