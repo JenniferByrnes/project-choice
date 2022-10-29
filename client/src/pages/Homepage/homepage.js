620212fdd0ebbb3a6b8d5fac032af4d39545de53
babaphillips <amandavphillips@icloud.com>
2022-10-29 19:17:35 -0400

    Fixing homepage

--- client/src/pages/Homepage/homepage.js
+++ client/src/pages/Homepage/homepage.js
@@ -2,6 +2,7 @@ import { motion } from "framer-motion";
 import mainLogo from "../../assets/mainLogo/fullLogo.png";
 import "./homepage.css";
 // bg-[#dae8e3]
+
 function Homepage() {
   return (
     <motion.div
@@ -33,96 +34,41 @@ function Homepage() {
         <div className="flex flex-col justify-center items-center">
           <div className="overflow-hidden border-8 border-double sm:rounded-md">
             <div className="bg-white px-4 py-5 sm:p-6">
-              <div className="font-extrabold leading-10">ABORTION IS HEALTH CARE.</div>
+              <div className="font-extrabold leading-10">
+                ABORTION IS HEALTH CARE.
+              </div>
               At Project Choice we believe in body autonomy and the womans
-              absolute right to chose. The last thing that should stand in your way is lack of information, and so our project was born.
-              <br/>
-              <br/>
-              By filling out this form you will be connected with clinics closest to you, laws in your area, and many other resources to help keep you safe and healthy. 
-              <br/>
-              <br/>
+              absolute right to chose. The last thing that should stand in your
+              way is lack of information, and so our project was born.
+              <br />
+              <br />
+              By filling out this form you will be connected with clinics
+              closest to you, laws in your area, and many other resources to
+              help keep you safe and healthy.
+              <br />
+              <br />
               If you have any additional questions, please contact us!
-
             </div>
           </div>
         </div>
-
-        {/* start form code */}
         <div className="flex flex-col justify-center items-center">
-          <form action="#" method="POST">
-            <div className="overflow-hidden border-8 border-double sm:rounded-md">
-              <div className="bg-white px-4 py-5 sm:p-6">
-                <div className="grid grid-cols-6 gap-6">
-                  <div className="col-span-6 sm:col-span-4">
-                    <label
-                      htmlFor="age"
-                      className="block text-sm font-medium text-gray-700"
-                    >
-                      Age
-                    </label>
-                    <input
-                      type="text"
-                      name="age"
-                      id="age"
-                      autoComplete="age"
-                      className="mt-1 block w-full rounded-md border-4 shadow-sm sm:text-sm"
-                    />
-                  </div>
-
-                  <div className="col-span-6 sm:col-span-3">
-                    <label
-                      htmlFor="country"
-                      className="block text-sm font-medium text-gray-700"
-                    >
-                      Time Since Last Period
-                    </label>
-                    <select
-                      id="period"
-                      className="mt-1 block w-full rounded-md border-4 bg-white py-2 px-3 shadow-sm  sm:text-sm"
-                    >
-                      <option></option>
-                      <option>Less than 6 weeks ago</option>
-                      <option>6 to 11 weeks ago</option>
-                      <option>12 to 15 weeks ago</option>
-                      <option>16 to 20 weeks ago</option>
-                      <option>More than 20 weeks ago</option>
-                      <option>Not sure</option>
-                    </select>
-                  </div>
-
-                  <div className="col-span-6">
-                    <label
-                      htmlFor="zip-code"
-                      className="block text-sm font-medium text-gray-700"
-                    >
-                      Zip Code
-                    </label>
-                    <input
-                      type="text"
-                      name="zip-code"
-                      id="zip-code"
-                      autoComplete="zip-code"
-                      className="mt-1 block w-full rounded-md border-4 shadow-sm sm:text-sm"
-                    />
-                  </div>
-                </div>
+          <div className="overflow-hidden border-8 border-double sm:rounded-md">
+            <div className="bg-white px-4 py-5 sm:p-6">
+              <div className="font-extrabold leading-10">
+                To find out more about abortion regulations in your State click
+                below.
               </div>
-              <div className="bg-gray-50 px-4 py-3 text-right sm:px-6">
-                <button
-                  type="submit"
-                  className="inline-flex justify-center rounded-md border border-transparent bg-red-600 py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-red-500 focus:ring-offset-2"
-                >
-                  Search
-                </button>
+              <div>
+                <button>Click</button>
               </div>
             </div>
-          </form>
+          </div>
         </div>
       </div>
+      {/* start form code */}
     </motion.div>
   );
 }
-
 
 // let zipCode = document.getElementById("zip-code").value;
 // console.log (zipCode);
@@ -132,11 +78,5 @@ function Homepage() {
 
 // let period = document.querySelector("#period").value;
 // console.log (period);
-
-
-
-
-
-
 
 export default Homepage;
