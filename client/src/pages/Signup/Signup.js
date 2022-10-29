64fbbdaa63ec3c533e989370c6fd5a68931e5658
Jennifer Byrnes <jennifer.byrnes@outlook.com>
2022-10-29 11:21:05 -0400

    signin/login

--- client/src/pages/Signup/Signup.js
+++ client/src/pages/Signup/Signup.js
@@ -13,9 +13,7 @@ function Signup(props) {
     const mutationResponse = await addUser({
       variables: {
         email: formState.email,
-        password: formState.password,
-        firstName: formState.firstName,
-        lastName: formState.lastName,
+        password: formState.password
       },
     });
     const token = mutationResponse.data.addUser.token;
@@ -35,30 +33,11 @@ function Signup(props) {
       <Link to="/login">← Go to Login</Link>
 
       <h2>Signup</h2>
-      <form onSubmit={handleFormSubmit}>
-        <div className="flex-row space-between my-2">
-          <label htmlFor="firstName">First Name:</label>
+      <form className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4" onSubmit={handleFormSubmit}>
+        <div className=" mb-4">
+          <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="email">Email:</label>
           <input
-            placeholder="First"
-            name="firstName"
-            type="firstName"
-            id="firstName"
-            onChange={handleChange}
-          />
-        </div>
-        <div className="flex-row space-between my-2">
-          <label htmlFor="lastName">Last Name:</label>
-          <input
-            placeholder="Last"
-            name="lastName"
-            type="lastName"
-            id="lastName"
-            onChange={handleChange}
-          />
-        </div>
-        <div className="flex-row space-between my-2">
-          <label htmlFor="email">Email:</label>
-          <input
+          className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" 
             placeholder="youremail@test.com"
             name="email"
             type="email"
@@ -66,18 +45,22 @@ function Signup(props) {
             onChange={handleChange}
           />
         </div>
-        <div className="flex-row space-between my-2">
-          <label htmlFor="pwd">Password:</label>
+        <div className="mb-6">
+          <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="pwd">Password:</label>
           <input
+          className="shadow appearance-none border border-red-500 rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
             placeholder="******"
             name="password"
             type="password"
             id="pwd"
             onChange={handleChange}
           />
+          <p className="text-red-500 text-xs italic">Please choose a password.</p>
         </div>
-        <div className="flex-row flex-end">
-          <button type="submit">Submit</button>
+        <div className="flex items-center justify-between">
+          <button 
+          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+          type="submit">Submit</button>
         </div>
       </form>
     </div>
