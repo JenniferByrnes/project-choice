f7d75b33e4cdefab615f81dc541f4e8b86513b8b
James-Fraser1 <jdfraser2022@gmail.com>
2022-10-30 13:31:00 -0400

    hover effects and styling for cards

--- client/src/components/ProductItem/index.js
+++ client/src/components/ProductItem/index.js
@@ -55,7 +55,7 @@ function ProductItem(item) {
         <div>{quantity} {pluralize("item", quantity)} in stock</div>
         <span>${price}</span>
       </div>
-      <button onClick={addToCart}>Add to cart</button>
+      <button className="btnedit" onClick={addToCart}>Add to cart</button>
     </div>
   );
 }
