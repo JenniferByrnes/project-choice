b35c6c6b2138332adbfd447bce772bfd4ce5d151
cortland.finch <cortlandfinch@Cortlands-MacBook-Pro.local>
2022-10-31 17:19:05 -0400

    fix about header styling

--- client/src/pages/About/about.js
+++ client/src/pages/About/about.js
@@ -8,9 +8,9 @@ function About() {
       name="about-tiles"
       className="w-full mb-6 p-6 md:h-screen text-[#2b2b2b]"
     >
-      <div className="mt-12 p-6 flex flex-col justify-center w-full h-full">
+      <div className="mt-12 p-6 flex flex-col justify-center w-full">
         <div className="about-heading pb-12 text-center">
-          <h2 className="text-4xl inline border-b-4 border-[#dc585d] py-2">
+          <h2 className="text-4xl inline border-b-4 border-[#dc585d] py-3">
             About Project Choice
           </h2>
         </div>
