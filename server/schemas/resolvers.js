babd108276778d47a9a72bb8989e9ed27994b763
babaphillips <amandavphillips@icloud.com>
2022-10-31 15:48:34 -0400

    Checkout stripe working

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,7 +1,8 @@
 const { AuthenticationError } = require("apollo-server-express");
 const { User, Product, Category, Order } = require("../models");
 const { signToken } = require("../utils/auth");
-const stripe = require("stripe")(SECRET_KEY);
+require("dotenv").config({ path: "./.env" });
+const stripe = require("stripe")(process.env.SECRET_KEY);
 
 const resolvers = {
   Query: {
