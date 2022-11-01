76ad6f530457dd36a84d421b28e55661ddec17a6
cortland.finch <cortlandfinch@Cortlands-MacBook-Pro.local>
2022-11-01 12:49:45 -0400

    refactor login and sign up page with new form styling

--- client/src/pages/Login/Login.js
+++ client/src/pages/Login/Login.js
@@ -31,7 +31,7 @@ function Login(props) {
   return (
   <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
       <h1 className="flex items-center mb-6 text-3xl text-black">
-          Project Choice 
+          Account Login
       </h1>
       <div className="w-full bg-white border-8 border-[#dae8e3] border-double shadow-lg shadow-[#dae8e3] md:mt-0 sm:max-w-md xl:p-0 items-center justify-center">
           <div className="p-6 space-y-4 md:space-y-6 sm:p-8 items-center justify-center">
