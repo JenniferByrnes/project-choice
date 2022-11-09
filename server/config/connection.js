6f6e0b37ecc4b82d9b3beb57ea67a8a9c4f39112
Jennifer Byrnes <jennifer.byrnes@outlook.com>
2022-11-09 17:50:47 -0500

    connection issues

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
 mongoose.connect(
-  process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/project-choices",
+  process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/test",
   {
     useNewUrlParser: true,
     useUnifiedTopology: true,
