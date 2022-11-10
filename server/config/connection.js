b715de564df12523f7f9de9b2f0d8f863d4a5b56
Jennifer Byrnes <jennifer.byrnes@outlook.com>
2022-11-10 08:49:29 -0500

    Update connection.js

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 
-// This is where we connect to out MongoDB
+// This is where we connect to out MongoDB//
 mongoose.connect(
   process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/project-choice",
   {
