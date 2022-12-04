47e2288e9924de93106e8daabc7f776221d9e39d
Jennifer Byrnes <jennifer.byrnes@outlook.com>
2022-12-04 18:36:16 -0500

    refactored policy components

--- client/src/components/RegulationsMinor/index.js
+++ client/src/components/RegulationsMinor/index.js
@@ -6,104 +6,59 @@ export default function RegulationsMinor(props) {
 
   // Check the data in the "banned_after_weeks_since_LMP field"
   function parentsRequired() {
-    return (
-      <div>
-        {(minorRegulations.parents_required === undefined) ?
-          // It's undefined (not present) - no statement needed
-          (<div></div>) :
-          // data is present
-          (<div>
-            <div>
-              {(() => {
-                switch (minorRegulations.parents_required) {
-                  case 1:
-                    return <li>One parent must be notified.</li>
-                  case 2:
-                    return <li>Both parents must be notified.</li>
-                  default:
-                    return <li>There are no restrictions on minors' abortions currently being enforced.</li>
-                }
-
-              })()}
-              <li>Many, but not all, states have extended the definition of "parent" to include other adult guardians. If parents meeting the state's definition cannot be located, a minor would often need to secure a judicial bypass.</li>
-            </div>
-          </div>)
-        }
-      </div>
-    )
+    const parentDef = <li>Many, but not all, states have extended the definition of "parent" to include other adult guardians. If parents meeting the state's definition cannot be located, a minor would often need to secure a judicial bypass.</li>
+    if (minorRegulations.parents_required === undefined)
+      return <li>There are no restrictions on minors' abortions currently being enforced.</li>
+    else {
+      switch (minorRegulations.parents_required) {
+        case 1:
+          return <><li>One parent must be notified.</li>{parentDef}</>
+        case 2:
+          return <><li>Both parents must be notified.</li>{parentDef}</>
+        default:
+          return <li>There are no restrictions on minors' abortions currently being enforced.</li>
+      }
+    }
   }
 
   // Check the text data in the "banned_after_weeks_since_LMP field"
   function belowAge() {
-    return (
-      <div>
-        {(minorRegulations.below_age === undefined) ?
-          // Yes, it's undefined (not present) - there are no restrictions
-          (<li>There are no restrictions on minor's abortions currently being enforced in {props.stateUS}.</li>) :
-          // data is present
-          <li>Abortions restrictions for minors end at age {minorRegulations.below_age}.</li>
-        }
-      </div>
-    )
+    if (minorRegulations.below_age === undefined)
+      return <li>There are no restrictions on minor's abortions currently being enforced in {props.stateUS}.</li>
+    else
+      return <li>Abortions restrictions for minors end at age {minorRegulations.below_age}.</li>
   }
+
   function parentalConsentRequired() {
-    return (
-      <div>
-        {(minorRegulations.parental_consent_required !== true) ?
-          // Not checked - no comment needed 
-          (<></>) :
-          // Yes, a parent or parents must give permission for the minor to have an abortion.
-          (<li>A parent or parents must give permission for the minor to have an abortion.</li>)
-        }
-      </div>
-    )
+    if (minorRegulations.parental_consent_required === true)
+      return <li>A parent or parents must give permission for the minor to have an abortion.</li>
   }
 
   // Check the data in the "exception_rape_or_incest field"
   function judicialBypassAvailable() {
-    return (
-      <div>
-        {(minorRegulations.judicial_bypass_available === undefined) ?
-          // It's undefined (not present) - no statement needed
-          (<div></div>) :
-          // data is present
-          (<div>
-            <div>
-              {(() => {
-                switch (minorRegulations.judicial_bypass_available) {
-                  case true:
-                    return <li>A judge can excuse a minor from the required parental consent and/or notification.</li>
-                  default:
-                    return <li>A judge cannot excuse a minor from the required parental consent and/or notification.</li>
-                }
-              })()}
-              <li>A judicial bypass is available in most states with restrictions, and in some the patient can be excused by a doctor.</li>
-            </div>
-          </div>)
-        }
-      </div>
-    )
+    if (minorRegulations.judicial_bypass_available === true)
+      return <li>A judge can excuse a minor from the required parental consent and/or notification.</li>
+    else
+      return <li>A judicial bypass is available in most states with restrictions, and in some the patient can be excused by a doctor.</li>
   }
 
   return (
-    <div>
-      <div>
-        <div className="text-xl p-3">Laws concerning Minors</div>
-        <ul className=" p-3 list-disc">
+    <>
+      <div className="text-xl p-3">Laws concerning Minors</div>
+      <ul className=" p-3 list-disc">
 
-          {/* Check exception_life */}
-          {belowAge()}
+        {/* Check exception_life */}
+        {belowAge()}
 
-          {/* Check exception_health */}
-          {parentsRequired()}
+        {/* Check exception_health */}
+        {parentsRequired()}
 
-          {/* Check parental_consent_required */}
-          {parentalConsentRequired()}
+        {/* Check parental_consent_required */}
+        {parentalConsentRequired()}
 
-          {/* Check for availablitiy of judicial bypass */}
-          {judicialBypassAvailable()}
+        {/* Check for availablitiy of judicial bypass */}
+        {judicialBypassAvailable()}
 
-        </ul>
-      </div>
-    </div>)
+      </ul>
+    </>)
 }
--- client/src/components/RegulationsWait/index.js
+++ client/src/components/RegulationsWait/index.js
@@ -7,96 +7,64 @@ export default function RegulationsWait(props) {
 
   // Check exceptionHealth
   function exceptionHealth() {
-    return (
-      <div>
-        {(waitingPeriodRegulations.exception_health === undefined) ?
-          // Yes, it's undefined (not present) - no data available
-          (<></>) :
-          // data is present
-
-          <li>The counseling requirement can be waived under certain conditions: {waitingPeriodRegulations.exception_health} </li>
-
-        }
-      </div>
-    )
+    if (waitingPeriodRegulations.exception_health === undefined)
+      return;
+    else
+      return (
+        <li>The counseling requirement can be waived under certain conditions: {waitingPeriodRegulations.exception_health} </li>
+      )
   }
 
   function waitingPeriodHours() {
-    return (
-      <div>
-        {(waitingPeriodRegulations.waiting_period_hours === undefined) ?
-          // Yes, it's undefined (not present) - no data available
-          (<li>{props.stateUS} does not have a mandatory waiting period.</li>) :
-          // data is present
-          <>
-            <li>{props.stateUS} requires a {waitingPeriodRegulations.waiting_period_hours} between receiving state mandated abortion counseling and obtaining an abortion. </li>
-            <li>Note that all states waive mandatory waiting period requirements in a medical emergency or when the woman's life or health is threatened. </li>
-          </>
-        }
-      </div>
-    )
+    if (waitingPeriodRegulations.waiting_period_hours === undefined)
+      return <li>{props.stateUS} does not have a mandatory waiting period.</li>;
+    else
+      return <>
+        <li>{props.stateUS} requires a {waitingPeriodRegulations.waiting_period_hours} between receiving state mandated abortion counseling and obtaining an abortion. </li>
+        <li>Note that all states waive mandatory waiting period requirements in a medical emergency or when the woman's life or health is threatened. </li>
+      </>
+
   }
+
   function waitingPeriodNotes() {
-    return (
-      <div>
-        {(waitingPeriodRegulations.waiting_period_notes === undefined) ?
-          // Yes, it's undefined (not present) - no data available
-          (<></>) :
-          // data is present
-
-          <li>Special notes about waiting periods for {props.stateUS}: {waitingPeriodRegulations.waiting_period_notes} </li>
-
-        }
-      </div>
-    )
+    if (waitingPeriodRegulations.waiting_period_notes === undefined)
+      return;
+    else
+      return <li>Special notes about waiting periods for {props.stateUS}: {waitingPeriodRegulations.waiting_period_notes} </li>
   }
 
   // Check the data in the "exception_rape_or_incest field"
   function counselingVisits() {
-    return (
-      <div>
-        {(waitingPeriodRegulations.counseling_visits === undefined) ?
-          // It's undefined (not present)
-          (<li>No counseling visits are required by the state.</li>) :
-          // data is present
-          (<div>
-            <div>
-              {(() => {
-                switch (waitingPeriodRegulations.counseling_visits) {
-                  case 1:
-                    return <li>Counseling is required by the state.</li>
-                  case 2:
-                    return <li>Abortion counseling or ultrasound must be obtained at the facility before the waiting period begins, requiring two trips to the clinic.</li>
-                  default:
-                    return <li>No counseling data is available.</li>
-                }
-              })()}
-            </div>
-          </div>)
-        }
-      </div>
-    )
+    if (waitingPeriodRegulations.counseling_visits === undefined)
+      return <li>No counseling visits are required by the state.</li>
+    else
+      switch (waitingPeriodRegulations.counseling_visits) {
+        case 1:
+          return <li>Counseling is required by the state.</li>
+        case 2:
+          return <li>Abortion counseling or ultrasound must be obtained at the facility before the waiting period begins, requiring two trips to the clinic.</li>
+        default:
+          return <li>No counseling data is available.</li>
+      }
   }
 
   return (
-    <div>
-      <div>
-        <div className="text-xl p-3">Laws about Waiting Periods</div>
-        <ul className=" p-3 list-disc">
+    <>
+      <div className="text-xl p-3">Laws about Waiting Periods</div>
+      <ul className=" p-3 list-disc">
 
-          {/* Check for waiting period hours requirement*/}
-          {waitingPeriodHours()}
+        {/* Check for waiting period hours requirement*/}
+        {waitingPeriodHours()}
 
-          {/* Check for required counseling visits() */}
-          {counselingVisits()}
+        {/* Check for required counseling visits() */}
+        {counselingVisits()}
 
-          {/* Check exception_health */}
-          {exceptionHealth()}
+        {/* Check exception_health */}
+        {exceptionHealth()}
 
-          {/* Check waitingPeriodNotes() */}
-          {waitingPeriodNotes()}
+        {/* Check waitingPeriodNotes() */}
+        {waitingPeriodNotes()}
 
-        </ul>
-      </div>
-    </div>)
+      </ul>
+    </>)
 }
