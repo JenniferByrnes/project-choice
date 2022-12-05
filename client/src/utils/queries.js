9089c1950592639898427e00646e5373bc1dddbc
babaphillips <amandavphillips@icloud.com>
2022-12-04 22:27:59 -0500

    merged main into seedingdata/final

--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -16,22 +16,6 @@ export const QUERY_PRODUCTS = gql`
   }
 `;
 
-export const QUERY_POLICY = gql`
-  query getPolicy($state: state) {
-    products(category: $category) {
-      _id
-      name
-      description
-      price
-      quantity
-      image
-      category {
-        _id
-      }
-    }
-  }
-`;
-
 export const QUERY_CHECKOUT = gql`
   query getCheckout($products: [ID]!) {
     checkout(products: $products) {
