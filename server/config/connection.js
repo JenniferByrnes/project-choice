12e15525d6491e4cf568d91b815f28819b329aa4
Jennifer Byrnes <jennifer.byrnes@outlook.com>
2022-12-09 14:29:46 -0500

    removed unneeded code

--- server/config/connection.js
+++ server/config/connection.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 // This is where we connect to our MongoDB//
 
-// process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/projectchoice",
+// process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/projectchoice" .  ,
 mongoose.connect(
   process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/project-choicess",
 // To run seeds - connect to MONGODB_URI - mongodb+srv://projectchoice: .... with no local option.
